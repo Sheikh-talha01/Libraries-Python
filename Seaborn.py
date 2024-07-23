a3358867ad469ed213709ade647bf9f1727d39ae
Talha Sheikh <167472327+Sheikh-talha01@users.noreply.github.com>
2024-07-23 21:03:27 +0500

    Update Seaborn.py

--- Seaborn.py
+++ Seaborn.py
@@ -9,3 +9,5 @@ tips = sns.load_dataset( "tips")
 # This data will be imported in the form of panda libaray data
 tips.head()
 # This will import top 5 rows from tips dataset
+tips.tail()
+# This will import 5 ending rows from tips dataset
